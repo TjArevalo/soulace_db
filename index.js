b422bcc6bb777a2fb5a9b3c346d38fd13f612ea1
TjArevalo <jimmyjamesyngson@gmail.com>
2022-07-07 18:59:39 -0700

    save

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const lyrics = require("./lyricData.json");
 
+
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Request Successful. Welcome to a piece of my mind.")
 });
 
 app.get("/lyrics", (req, res) => {
-    let sending = JSON.parse(lyrics)
-    res.send(sending)
+    res.send(lyrics)
 })
 
 app.listen(8080, () => {
